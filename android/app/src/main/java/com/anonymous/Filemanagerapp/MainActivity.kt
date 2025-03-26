a67bf27f57dc6ac17a351616dc363c43607869ee
khemachat sittiritkawin <157001334+khemachat-sittiritkawin@users.noreply.github.com>
2025-03-26 21:54:56 +0700

    rename scheme

--- android/app/src/main/java/com/anonymous/Filemanagerapp/MainActivity.kt
+++ android/app/src/main/java/com/anonymous/Filemanagerapp/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ReactActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     // Set the theme to AppTheme BEFORE onCreate to support
     // coloring the background, status bar, and navigation bar.
-    // This is required for expo-splash-screen.
+    // This is required for expo-splash-screen
     setTheme(R.style.AppTheme);
     super.onCreate(null)
   }
