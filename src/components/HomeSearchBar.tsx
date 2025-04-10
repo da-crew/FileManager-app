ccf94b4ba2812fcf57a13c7b2dfe32fc4220ea70
PokpongRomsri <6631503026@lamduan.mfu.ac.th>
2025-04-11 02:03:41 +0700

    can open in home screen now

--- src/components/HomeSearchBar.tsx
+++ src/components/HomeSearchBar.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import { View, TouchableOpacity } from 'react-native';
 import { Octicons, Ionicons } from '@expo/vector-icons';
-
 import { NavigationProp } from '@react-navigation/native';
+import { Path } from '../FileSystem';
+import { ContainerType } from './ContentContainer/common';
+import RNFS from 'react-native-fs';
 
 export interface HomeSearchBarProps {
     navigation: NavigationProp<any>;
 }
 
 const HomeSearchBar: React.FC<HomeSearchBarProps> = ({ navigation }) => {
+    const handleSearch = () => {
+        const rootPath = new Path('Internal Storage', RNFS.ExternalStorageDirectoryPath, []);
+        navigation.navigate("Search", {
+            containerName: 'Internal Storage',
+            path: rootPath,
+            containerType: ContainerType.DEFAULT
+        });
+    };
+
     return (
         <View style={{ padding: 20, paddingBottom: 0 }}>
             <View style={{ flexDirection: 'row' }}>
                 <TouchableOpacity
                     style={{ marginLeft: 'auto' }}
-                    onPress={() => navigation.navigate("Search")}
+                    onPress={handleSearch}
                 >
                     <Ionicons name="search" size={24} color="black" />
                 </TouchableOpacity>
